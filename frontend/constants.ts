ba8bc866844b5baa395b73b0e590d9cf55b2f227
GideonBature <infoaboutgideon@gmail.com>
2024-09-04 18:24:36 +0100

    incorporated nodit into dapp

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,1 +1,2 @@
-export const NETWORK = import.meta.env.VITE_APP_NETWORK ?? "testnet";
+export const NETWORK = (import.meta as any).env.VITE_APP_NETWORK ?? "testnet";
+export const API_KEY = (import.meta as any).env.VITE_APP_API_KEY ?? "aptos";
